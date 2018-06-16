5b932c85b98d2e1d5b289aa02f0f148a39c91add
dzikoysk <dzikoysk@dzikoysk.net>
2018-06-16 17:25:48 +0200

    Reformat code

--- src/main/java/net/dzikoysk/reposilite/ReposiliteApplication.java
+++ src/main/java/net/dzikoysk/reposilite/ReposiliteApplication.java
@@ -1,13 +1,13 @@
 package net.dzikoysk.reposilite;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
 public class ReposiliteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReposiliteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReposiliteApplication.class, args);
+    }
 
 }
