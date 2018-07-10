96cdcc3a1fa4ae23acfa1c1d94a62930bf4ed5e4
jaqobb <jaqobb@jaqobb.co>
2018-07-10 12:04:49 +0200

    Revert some previous changes and add a few todo as reminders

--- src/main/java/net/dzikoysk/reposilite/web/RepositoryController.java
+++ src/main/java/net/dzikoysk/reposilite/web/RepositoryController.java
@@ -2,6 +2,7 @@ package net.dzikoysk.reposilite.web;
 
 import net.dzikoysk.reposilite.domain.depository.Artifact;
 import net.dzikoysk.reposilite.domain.depository.DepositoryEntity;
+import net.dzikoysk.reposilite.service.common.UserService;
 import net.dzikoysk.reposilite.service.depository.ArtifactService;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,32 +18,40 @@ import org.springframework.web.servlet.HandlerMapping;
 public class RepositoryController {
 
     private final ArtifactService artifactService;
+    private final UserService userService;
     private final AntPathMatcher apm = new AntPathMatcher();
 
-    public RepositoryController(@Autowired ArtifactService artifactService) {
+    public RepositoryController(@Autowired ArtifactService artifactService, @Autowired UserService userService) {
         this.artifactService = artifactService;
+        this.userService = userService;
     }
 
     @RequestMapping
     @ResponseBody
     public String repositories() {
+        //TODO: Return only public repositories and hidden ones as well if user is logged and have permission to them.
+
         return "Repository list";
     }
 
     @RequestMapping("/{repository}")
     @ResponseBody
     public String repository(@PathVariable("repository") String repository) {
+        //TODO: Make sure repository exists.
+
         return "Repository name: " + repository;
     }
 
     @RequestMapping("/{repository}/**")
     @ResponseBody
     public String repository(@PathVariable("repository") String repository, HttpServletRequest request) {
+        //TODO: Make sure repository exists.
+
         String path = request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE).toString();
         String bestMatchingPattern = request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE).toString();
-        String finalPath = this.apm.extractPathWithinPattern(bestMatchingPattern, path);
+        String finalPath = apm.extractPathWithinPattern(bestMatchingPattern, path);
 
-        DepositoryEntity entity = this.artifactService.getDepositoryEntity(finalPath);
+        DepositoryEntity entity = artifactService.getDepositoryEntity(finalPath);
         if (entity == null) {
             //TODO: Entity (artifact) not found, perhaps display some 404 page?
             return "Artifact: 404";
