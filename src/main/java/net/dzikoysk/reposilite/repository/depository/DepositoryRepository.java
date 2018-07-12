7792fef889591aabaeb5f8af399c0325af1f0a86
jaqobb <jaqobb@jaqobb.co>
2018-07-12 16:11:03 +0200

    Use spring's annotations instead of jetbrains'

--- src/main/java/net/dzikoysk/reposilite/repository/depository/DepositoryRepository.java
+++ src/main/java/net/dzikoysk/reposilite/repository/depository/DepositoryRepository.java
@@ -2,19 +2,18 @@ package net.dzikoysk.reposilite.repository.depository;
 
 import net.dzikoysk.reposilite.domain.depository.Depository;
 import net.dzikoysk.reposilite.domain.depository.DepositoryEntity;
-import org.jetbrains.annotations.Nullable;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class DepositoryRepository {
 
-    public @Nullable Depository findDepositoryByName(String name) {
-        //TODO: Make sure name is not null?
+    public @Nullable Depository findDepositoryByName(@NonNull String name) {
         return null;
     }
 
-    public @Nullable DepositoryEntity findEntityByPath(String path) {
-        //TODO: Make sure path is not null?
+    public @Nullable DepositoryEntity findEntityByPath(@NonNull String path) {
         return null;
     }
 
