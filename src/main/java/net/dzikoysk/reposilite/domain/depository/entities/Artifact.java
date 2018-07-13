bdc6bb4ec51380e0245b7de39cb5e5f4b464744d
jaqobb <jaqobb@jaqobb.co>
2018-07-13 18:39:32 +0200

    Playing around artifact and build data classes

--- src/main/java/net/dzikoysk/reposilite/domain/depository/entities/Artifact.java
+++ src/main/java/net/dzikoysk/reposilite/domain/depository/entities/Artifact.java
@@ -1,16 +1,52 @@
 package net.dzikoysk.reposilite.domain.depository.entities;
 
 import net.dzikoysk.reposilite.domain.depository.DepositoryEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
+import java.io.File;
 import java.util.Collection;
+import java.util.Collections;
 
 public class Artifact implements DepositoryEntity {
 
     private String name;
     private Collection<Build> builds;
 
-    public synchronized void updateLocalMetadata() {
+    public String getName() {
+        return name;
+    }
 
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    public Collection<Build> getBuilds() {
+        return Collections.unmodifiableCollection(builds);
+    }
+
+    public void addBuild(@NonNull Build build) {
+        builds.add(build);
+    }
+
+    public void removeBuild(@NonNull Build build) {
+        //TODO: Delete build content?
+        builds.remove(build);
+    }
+
+    public void clearBuilds() {
+        builds.clear();
+    }
+
+    public @Nullable Build findBuildByVersion(@NonNull String version) {
+        return builds.stream()
+                .filter(build -> build.getVersion().equals(version))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public @Nullable Build findBuildByContent(@NonNull File file) {
+        return null;
     }
 
 }
--- src/main/java/net/dzikoysk/reposilite/domain/depository/entities/Build.java
+++ src/main/java/net/dzikoysk/reposilite/domain/depository/entities/Build.java
@@ -1,13 +1,44 @@
 package net.dzikoysk.reposilite.domain.depository.entities;
 
 import net.dzikoysk.reposilite.domain.depository.DepositoryEntity;
+import org.springframework.lang.NonNull;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.Collections;
 
 public class Build implements DepositoryEntity {
 
     private String version;
     private Collection<File> content;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Collection<File> getContent() {
+        return Collections.unmodifiableCollection(content);
+    }
+
+    public void addContent(@NonNull File file) {
+        content.add(file);
+    }
+
+    public void removeContent(@NonNull File file) {
+        //TODO: Delete file?
+        content.remove(file);
+    }
+
+    public void clearContent() {
+        //TODO: Probably uncomment if files are gonna be deleted.
+        //for (File file : content) {
+        //    removeContent(file);
+        //}
+        content.clear();
+    }
 
 }
