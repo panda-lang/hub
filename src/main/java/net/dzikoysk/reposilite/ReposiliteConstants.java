bbcf4d1059c38377c03ad1faf4a2f3fb2d7f1528
dzikoysk <dzikoysk@dzikoysk.net>
2018-08-12 14:09:06 +0200

    Update Spring Boot and version of Reposilite to indev-2018.8.12

--- src/main/java/net/dzikoysk/reposilite/ReposiliteConstants.java
+++ src/main/java/net/dzikoysk/reposilite/ReposiliteConstants.java
@@ -5,7 +5,7 @@ import org.springframework.core.SpringVersion;
 
 public final class ReposiliteConstants {
 
-    public static final String REPOSILITE_VERSION = "indev-0.0.2";
+    public static final String REPOSILITE_VERSION = "indev-2018.8.12";
 
     public static final String SPRING_BOOT_VERSION = SpringBootVersion.getVersion();
 
