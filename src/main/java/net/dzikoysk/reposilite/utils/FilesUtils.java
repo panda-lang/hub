35f1f39f5830da959188acb94a239efb4c2ed5fd
insertt <inserttdev@gmail.com>
2018-10-08 18:32:28 +0200

    Fix file lookup handling in directories (#13)

--- src/main/java/net/dzikoysk/reposilite/utils/FilesUtils.java
+++ src/main/java/net/dzikoysk/reposilite/utils/FilesUtils.java
@@ -8,14 +8,9 @@ public class FilesUtils {
 
     public static TreeNode<File> collectFiles(File directory) {
         TreeNode<File> tree = new TreeNode<>(directory);
-
-        if (!directory.isDirectory()) {
-            return tree;
-        }
-
         File[] files = directory.listFiles();
 
-        if (files == null) {
+        if (files == null || files.length == 0) {
             return tree;
         }
 
