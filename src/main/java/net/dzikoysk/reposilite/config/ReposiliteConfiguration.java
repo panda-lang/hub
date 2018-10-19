ef41a800749ddc3a61d078418e616782a47db912
Bartłomiej Mazur <GotoFinal@users.noreply.github.com>
2018-10-19 14:54:54 +0200

    Add shell configuration (#10) (#18)

--- src/main/java/net/dzikoysk/reposilite/config/ReposiliteConfiguration.java
+++ src/main/java/net/dzikoysk/reposilite/config/ReposiliteConfiguration.java
@@ -1,7 +1,10 @@
 package net.dzikoysk.reposilite.config;
 
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.shell.jline.PromptProvider;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.io.File;
@@ -15,4 +18,8 @@ public class ReposiliteConfiguration {
         return new File("repositories");
     }
 
+    @Bean
+    public PromptProvider shellPrompt() {
+        return () -> new AttributedString("reposilite:> ", AttributedStyle.DEFAULT.foreground(AttributedStyle.CYAN));
+    }
 }
