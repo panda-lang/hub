1382acb11f43c5596ed934426ed9c520e99a81e6
unknown <bstefanskicom@gmail.com>
2018-11-09 22:55:28 +0100

    Add the rest of unit tests (Closes #29)

---
+++ reposilite-backend/src/test/java/org.panda_lang.reposilite/depository/DepositoryServiceTest.java
@@ -0,0 +1,29 @@
+package org.panda_lang.reposilite.depository;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DepositoryServiceTest {
+
+    @Autowired
+    private DepositoryService depositoryService;
+
+    @Test
+    public void getDepositoryTest() {
+        Assert.assertNotNull(this.depositoryService.getDepository("releases"));
+    }
+
+    @Test
+    public void getDepositoryEntityTest() {
+        Depository depository = this.depositoryService.getDepository("releases");
+        DepositoryEntity depositoryEntity = this.depositoryService.getDepositoryEntity(depository, "org/panda-lang/panda-utilities");
+
+        Assert.assertNotNull(depositoryEntity);
+    }
+}
