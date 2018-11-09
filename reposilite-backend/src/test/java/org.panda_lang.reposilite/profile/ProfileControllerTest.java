95becf80b8517476fde904110da6e3c8dc102d70
unknown <bstefanskicom@gmail.com>
2018-11-09 14:22:05 +0100

    Cover controllers with unit tests #29

---
+++ reposilite-backend/src/test/java/org.panda_lang.reposilite/profile/ProfileControllerTest.java
@@ -0,0 +1,45 @@
+package org.panda_lang.reposilite.profile;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ProfileControllerTest {
+
+    @Autowired
+    private ProfileController profileController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(this.profileController).build();
+    }
+
+    @Test
+    public void profileTest() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/jkowalski/"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.account", Matchers.is("jkowalski")));
+    }
+
+    @Test
+    public void profileWithProjectTest() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/jkowalski/panda/"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.account", Matchers.is("jkowalski")))
+                .andExpect(jsonPath("$.project", Matchers.is("panda")));
+    }
+
+}
