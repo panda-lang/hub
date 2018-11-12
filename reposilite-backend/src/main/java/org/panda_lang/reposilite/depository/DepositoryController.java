6b821531e934d4fb58693661859978d7f7604386
unknown <bstefanskicom@gmail.com>
2018-11-12 22:37:16 +0100

    Cover rest controllers with docs

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/depository/DepositoryController.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/depository/DepositoryController.java
@@ -1,5 +1,9 @@
 package org.panda_lang.reposilite.depository;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.panda_lang.reposilite.depository.build.Data;
 import org.panda_lang.reposilite.utils.RequestUtils;
 import org.panda_lang.panda.utilities.commons.IOUtils;
@@ -18,6 +22,7 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@ApiOperation("Operations pertaining to repository")
 @RequestMapping("api/repository")
 @RestController
 public class DepositoryController {
@@ -29,16 +34,21 @@ public class DepositoryController {
         this.depositoryService = depositoryService;
     }
 
+    @ApiOperation("Print available repositories")
     @GetMapping
     public ResponseEntity<Map<String, Object>> repositories() {
-        //TODO: Return only public repositories and hidden ones as well if user is logged in and have permission to them.
         return ResponseEntity.ok(new LinkedHashMap<String, Object>() {{
             depositoryService.getNames().forEach(name -> put("name", name));
         }});
     }
 
+    @ApiOperation("Return name of repository")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Repository not found"),
+            @ApiResponse(code = 200, message = "Successfully returned")
+    })
     @GetMapping("/{repository}")
-    public ResponseEntity<Map<String, Object>> repository(@PathVariable String repository) {
+    public ResponseEntity<Map<String, Object>> repository(@PathVariable @ApiParam("Repository name") String repository) {
         if (this.depositoryService.getDepository(repository) == null) {
             return ResponseEntity.notFound().build();
         }
@@ -46,8 +56,13 @@ public class DepositoryController {
         return ResponseEntity.ok(Collections.singletonMap("name", repository));
     }
 
+    @ApiOperation("Return repository entity")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Repository entity not found"),
+            @ApiResponse(code = 200, message = "Successfully returned")
+    })
     @GetMapping("/{repository}/**")
-    public ResponseEntity<Object> repository(@PathVariable String repository, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public ResponseEntity<Object> repository(@PathVariable @ApiParam("Repository name") String repository, HttpServletRequest request, HttpServletResponse response) throws Exception {
         Depository depository = this.depositoryService.getDepository(repository);
         String entityQualifier = RequestUtils.extractWildcard(request);
         DepositoryEntity entity = depositoryService.getDepositoryEntity(depository, entityQualifier);
--- reposilite-backend/src/main/java/org/panda_lang/reposilite/profile/ProfileController.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/profile/ProfileController.java
@@ -1,5 +1,7 @@
 package org.panda_lang.reposilite.profile;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,17 +9,20 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@ApiOperation("Operations pertaining to profile")
 @RestController
 @RequestMapping("api/account/{account}")
 public class ProfileController {
 
+    @ApiOperation("Print profile name")
     @GetMapping
-    public ResponseEntity<Map<String, Object>> profile(@PathVariable String account) {
+    public ResponseEntity<Map<String, Object>> profile(@PathVariable @ApiParam("Account name") String account) {
         return ResponseEntity.ok(Collections.singletonMap("account", account));
     }
 
+    @ApiOperation("Print profile and project name")
     @GetMapping("/{project}")
-    public ResponseEntity<Map<String, Object>> project(@PathVariable String account, @PathVariable String project) {
+    public ResponseEntity<Map<String, Object>> project(@PathVariable @ApiParam("Account name") String account, @PathVariable @ApiParam("Project name") String project) {
         return ResponseEntity.ok(new LinkedHashMap<String, Object>() {{
             put("account", account);
             put("project", project);
