387b5f7ac7c7b8a5e8828a3d1ad85008f2be9693
unknown <bstefanskicom@gmail.com>
2018-11-12 22:36:19 +0100

    Set up swagger

---
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/swagger/SwaggerConfiguration.java
@@ -0,0 +1,33 @@
+package org.panda_lang.reposilite.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Profile("development")
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    private static final String PATH_REGEX = "(?!.*error).*$";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.regex(PATH_REGEX))
+                .build()
+                .apiInfo(new ApiInfoBuilder()
+                        .title("Reposilite API Documentation")
+                        .version("1.0")
+                        .contact(new Contact("Panda", "https://panda-lang.org/", "panda@panda-lang.org"))
+                        .build());
+    }
+
+}
