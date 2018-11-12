088380a6c8c8682ea29f550d4ed283de89dd8e7b
unknown <bstefanskicom@gmail.com>
2018-11-12 14:47:09 +0100

    Update spring security configuration (Closes #11)

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/config/ReposiliteConfiguration.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/config/ReposiliteConfiguration.java
@@ -4,6 +4,8 @@ import org.jline.utils.AttributedString;
 import org.jline.utils.AttributedStyle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.shell.jline.PromptProvider;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -21,6 +23,11 @@ public class ReposiliteConfiguration {
     @Bean
     public PromptProvider shellPrompt() {
         return () -> new AttributedString("reposilite:> ", AttributedStyle.DEFAULT.foreground(AttributedStyle.CYAN));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- reposilite-backend/src/main/java/org/panda_lang/reposilite/config/WebSecurityConfiguration.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/config/WebSecurityConfiguration.java
@@ -1,13 +1,13 @@
 package org.panda_lang.reposilite.config;
 
-import org.springframework.context.annotation.Bean;
+import org.panda_lang.reposilite.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
@@ -15,16 +15,24 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private final UserService userService;
+
+    @Autowired
+    public WebSecurityConfiguration(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/**").permitAll();
+                .antMatchers(HttpMethod.POST, "/**").permitAll()
+                .and().httpBasic();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userService);
     }
 
 }
