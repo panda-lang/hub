3355aaba3d355ebb0f362b72a9157ae5ebdd9416
unknown <bstefanskicom@gmail.com>
2018-11-12 01:18:32 +0100

    Implement form request validation

---
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/auth/FieldNotNullAndEmpty.java
@@ -0,0 +1,16 @@
+package org.panda_lang.reposilite.auth;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@NotEmpty
+@NotNull
+public @interface FieldNotNullAndEmpty {
+
+}
