a3886b30022c36abf44a240f0b8cf3afaabbfd87
unknown <bstefanskicom@gmail.com>
2018-11-20 20:29:36 +0100

    Create custom user details & principal annotation

---
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/user/AuthenticatedUser.java
@@ -0,0 +1,15 @@
+package org.panda_lang.reposilite.user;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal(expression = "user")
+public @interface AuthenticatedUser {
+
+}
