f35b80f373dad4d8524d6b77928855669e15649d
unknown <bstefanskicom@gmail.com>
2018-11-21 14:11:28 +0100

    Add custom security annotations & secure organization controller (Closes #62)

---
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/utils/security/IsUser.java
@@ -0,0 +1,15 @@
+package org.panda_lang.reposilite.utils.security;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole('USER')")
+public @interface IsUser {
+
+}
--- reposilite-backend/src/test/java/org.panda_lang.reposilite/ReposiliteApplicationTest.java
+++ reposilite-backend/src/test/java/org.panda_lang.reposilite/ReposiliteApplicationTest.java
@@ -4,7 +4,6 @@ import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.panda_lang.reposilite.config.ReposiliteConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
