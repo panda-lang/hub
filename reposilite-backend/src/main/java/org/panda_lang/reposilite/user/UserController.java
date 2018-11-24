b7b2c481d76d2bc03b7857f6f643c5fa8f10afcd
unknown <bstefanskicom@gmail.com>
2018-11-24 19:25:31 +0100

    Implement CRUD actions to user controller

---
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/user/UserController.java
@@ -0,0 +1,19 @@
+package org.panda_lang.reposilite.user;
+
+import io.swagger.annotations.ApiOperation;
+import org.panda_lang.reposilite.utils.rest.AbstractCrudRestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@ApiOperation("Operations pertaining to user")
+@RestController
+@RequestMapping("api/user/")
+public class UserController extends AbstractCrudRestController<User> {
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        super(userRepository);
+    }
+
+}
