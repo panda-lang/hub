335d4d915f0a01c2b2142edc52dae45ecdc93546
unknown <bstefanskicom@gmail.com>
2018-12-01 15:39:19 +0100

    Improve files utils with method responsible for storing files

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/utils/FilesUtils.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/utils/FilesUtils.java
@@ -1,8 +1,14 @@
 package org.panda_lang.reposilite.utils;
 
 import org.panda_lang.reposilite.utils.collection.TreeNode;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class FilesUtils {
 
@@ -19,6 +25,17 @@ public class FilesUtils {
         }
 
         return tree;
+    }
+
+    public static void storeFile(Path path, MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        Path directoryPath = path.resolve(fileName);
+
+        try {
+            Files.copy(file.getInputStream(), directoryPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
 }
