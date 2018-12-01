96f91eb8a996e1f7a88c879166a9598b041b4cab
unknown <bstefanskicom@gmail.com>
2018-12-01 15:40:10 +0100

    Implement artifacts uploading & docs fixes (Closes #75)

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/depository/DepositoryController.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/depository/DepositoryController.java
@@ -4,7 +4,16 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.panda_lang.reposilite.depository.artifact.Artifact;
+import org.panda_lang.reposilite.depository.artifact.ArtifactFactory;
+import org.panda_lang.reposilite.depository.build.Build;
+import org.panda_lang.reposilite.depository.build.BuildFactory;
 import org.panda_lang.reposilite.depository.build.Data;
+import org.panda_lang.reposilite.depository.group.Group;
+import org.panda_lang.reposilite.depository.group.GroupFactory;
+import org.panda_lang.reposilite.utils.FilesUtils;
 import org.panda_lang.reposilite.utils.RequestUtils;
 import org.panda_lang.panda.utilities.commons.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +21,20 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -27,14 +44,14 @@ import java.util.Map;
 @RestController
 public class DepositoryController {
 
-    private final DepositoryServiceImpl depositoryService;
+    private final DepositoryService depositoryService;
 
     @Autowired
-    DepositoryController(DepositoryServiceImpl depositoryService) {
+    DepositoryController(DepositoryService depositoryService) {
         this.depositoryService = depositoryService;
     }
 
-    @ApiOperation("Print available repositories")
+    @ApiOperation("Prints available repositories")
     @GetMapping
     public ResponseEntity<Map<String, Object>> repositories() {
         return ResponseEntity.ok(new LinkedHashMap<String, Object>() {{
@@ -42,7 +59,7 @@ public class DepositoryController {
         }});
     }
 
-    @ApiOperation("Return name of repository")
+    @ApiOperation("Returns name of repository")
     @ApiResponses({
             @ApiResponse(code = 404, message = "Repository not found"),
             @ApiResponse(code = 200, message = "Successfully returned")
@@ -56,7 +73,7 @@ public class DepositoryController {
         return ResponseEntity.ok(Collections.singletonMap("name", repository));
     }
 
-    @ApiOperation("Return repository entity")
+    @ApiOperation("Returns repository entity")
     @ApiResponses({
             @ApiResponse(code = 404, message = "Repository entity not found"),
             @ApiResponse(code = 200, message = "Successfully returned")
@@ -80,6 +97,45 @@ public class DepositoryController {
 
         File currentFile = new File(depository.getRootFile() + "/" + entityQualifier);
         return ResponseEntity.ok(new DepositoryResponse(currentFile.getPath(), currentFile.list()));
+    }
+
+    @ApiOperation("Adds artifact")
+    @ApiResponses({
+            @ApiResponse(code = 403, message = "File extensions not allowed"),
+            @ApiResponse(code = 403, message = "Url doesn't match with Artifact's format"),
+            @ApiResponse(code = 204, message = "Successfully updated")
+    })
+    @PutMapping("/{repository}/**")
+    public ResponseEntity<Object> addArtifact(@PathVariable String repository, @RequestBody MultipartFile file, HttpServletRequest request) throws IOException {
+        if (!Arrays.asList("jar", "xml", "pom").contains(FilenameUtils.getExtension(file.getOriginalFilename()))) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Depository depository = this.depositoryService.getDepository(repository);
+        String entityQualifier = RequestUtils.extractWildcard(request);
+
+        DepositoryEntity entity = this.depositoryService.getDepositoryEntity(depository, entityQualifier);
+        DepositoryPath project = DepositoryPath.ofSystemPath(Paths.get("\\" + entityQualifier).toString());
+
+        Path buildDirectoryPath = Paths.get(depository.getRootFile().getPath() + File.separator + entityQualifier);
+
+        if (entity == null && !StringUtils.isBlank(project.getBuildVersion())) {
+            Group group = new GroupFactory(depository).obtainGroup(project.getGroupName());
+            Artifact artifact = new ArtifactFactory(group).obtainArtifact(project.getArtifactName());
+            Build build = new BuildFactory(artifact).obtainBuild(project.getBuildVersion());
+
+            Files.createDirectories(buildDirectoryPath);
+            FilesUtils.storeFile(buildDirectoryPath, file);
+
+            return ResponseEntity.noContent().build();
+        }
+
+        if (entity instanceof Build) {
+            FilesUtils.storeFile(buildDirectoryPath, file);
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.badRequest().build();
     }
 
 }
