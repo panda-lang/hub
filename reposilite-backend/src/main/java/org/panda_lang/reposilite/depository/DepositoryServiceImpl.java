9a75262b63fc20fe973a96012a1c0e8f72b9ab47
unknown <bstefanskicom@gmail.com>
2018-12-02 22:43:03 +0100

    Create maven metadata's POJO & improve depository service

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/depository/DepositoryServiceImpl.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/depository/DepositoryServiceImpl.java
@@ -1,9 +1,18 @@
 package org.panda_lang.reposilite.depository;
 
+import org.panda_lang.reposilite.depository.artifact.Artifact;
+import org.panda_lang.reposilite.depository.group.Group;
+import org.panda_lang.reposilite.utils.FilesUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -25,6 +34,29 @@ public class DepositoryServiceImpl implements DepositoryService {
     @Override
     public @Nullable DepositoryEntity getDepositoryEntity(Depository depository, String entityQualifier) {
         return depositoryRepository.findEntityByURLPath(depository, entityQualifier);
+    }
+
+    @Override
+    public void generateMetaDataFile(Depository depository, Group group, Artifact artifact, Path buildDirectoryPath) {
+        File mostRecentDirectory = FilesUtils.getMostRecentDirectory(buildDirectoryPath.toAbsolutePath());
+        MavenMetadataFile metadata = new MavenMetadataFileBuilder()
+                .withGroupId(group.getName())
+                .withArtifactId(artifact.getName())
+                .withLatest(mostRecentDirectory.getName())
+                .withLastUpdated(mostRecentDirectory.lastModified())
+                .withVersions(new HashSet<>(artifact.getVersions()))
+                .withRelease(mostRecentDirectory.getName())
+                .build();
+
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(MavenMetadataFile.class);
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            jaxbMarshaller.marshal(metadata, new File(buildDirectoryPath.toString() + File.separator + "maven-metadata.xml"));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
---
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/depository/MavenMetadataFile.java
@@ -0,0 +1,126 @@
+package org.panda_lang.reposilite.depository;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+import java.util.Collection;
+
+@XmlRootElement(name = "metadata")
+@XmlAccessorType(XmlAccessType.FIELD)
+class MavenMetadataFile implements Serializable {
+
+    private String groupId;
+    private String artifactId;
+    private Versioning versioning;
+
+    MavenMetadataFile() {
+    }
+
+    MavenMetadataFile(String groupId, String artifactId, Versioning versioning) {
+        this.groupId = groupId;
+        this.artifactId = artifactId;
+        this.versioning = versioning;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public void setVersioning(Versioning versioning) {
+        this.versioning = versioning;
+    }
+
+    public String getGroupId() {
+        return this.groupId;
+    }
+
+    public String getArtifactId() {
+        return this.artifactId;
+    }
+
+    public Versioning getVersioning() {
+        return this.versioning;
+    }
+
+    @XmlRootElement(name = "versioning")
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Versioning {
+
+        private String release;
+        private String latest;
+        private Versions versions;
+        private long lastUpdated;
+
+        Versioning() {
+        }
+
+        Versioning(String release, String latest, Versions versions, long lastUpdated) {
+            this.release = release;
+            this.latest = latest;
+            this.versions = versions;
+            this.lastUpdated = lastUpdated;
+        }
+
+        public void setRelease(String release) {
+            this.release = release;
+        }
+
+        public void setLatest(String latest) {
+            this.latest = latest;
+        }
+
+        public void setVersions(Versions versions) {
+            this.versions = versions;
+        }
+
+        public void setLastUpdated(long lastUpdated) {
+            this.lastUpdated = lastUpdated;
+        }
+
+        public String getRelease() {
+            return this.release;
+        }
+
+        public String getLatest() {
+            return this.latest;
+        }
+
+        public Versions getVersions() {
+            return this.versions;
+        }
+
+        public long getLastUpdated() {
+            return this.lastUpdated;
+        }
+
+    }
+
+    @XmlRootElement
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Versions {
+
+        private Collection<String> version;
+
+        Versions() {
+        }
+
+        Versions(Collection<String> version) {
+            this.version = version;
+        }
+
+        public Collection<String> getVersion() {
+            return this.version;
+        }
+
+        public void setVersion(Collection<String> version) {
+            this.version = version;
+        }
+
+    }
+
+}
