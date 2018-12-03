71e53d0fbc2f16afa7415db430e1beeeb8901855
unknown <bstefanskicom@gmail.com>
2018-12-04 00:58:11 +0100

    Documentation for crud rest operations (Closes #69)

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/organization/OrganizationController.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/organization/OrganizationController.java
@@ -1,13 +1,8 @@
 package org.panda_lang.reposilite.organization;
 
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.panda_lang.reposilite.utils.rest.AbstractCrudRestController;
-import org.panda_lang.reposilite.utils.security.IsUser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,18 +14,6 @@ public class OrganizationController extends AbstractCrudRestController<Organizat
     @Autowired
     public OrganizationController(OrganizationRepository organizationRepository) {
         super(organizationRepository);
-    }
-
-    @IsUser
-    @ApiOperation(value = "Create an organization")
-    @ApiResponses({
-            @ApiResponse(code = 409, message = "Organization with that name already exists"),
-            @ApiResponse(code = 400, message = "Bad Request"),
-            @ApiResponse(code = 201, message = "Successfully created organization", response = Organization.class)
-    })
-    @Override
-    public ResponseEntity<Organization> create(Organization entity, BindingResult result) {
-        return super.create(entity, result);
     }
 
 }
--- reposilite-backend/src/main/java/org/panda_lang/reposilite/utils/rest/AbstractCrudRestController.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/utils/rest/AbstractCrudRestController.java
@@ -1,5 +1,9 @@
 package org.panda_lang.reposilite.utils.rest;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.apache.commons.beanutils.BeanUtilsBean;
 import org.bson.types.ObjectId;
 import org.panda_lang.reposilite.utils.IdentifiableEntity;
@@ -32,6 +36,11 @@ public abstract class AbstractCrudRestController<T extends IdentifiableEntity<Ob
         this.repository = repository;
     }
 
+    @ApiOperation("Creates new entity")
+    @ApiResponses({
+            @ApiResponse(code = 409, message = "Entity already exists"),
+            @ApiResponse(code = 201, message = "Successfully created entity")
+    })
     @PostMapping
     public ResponseEntity<T> create(@RequestBody @Valid T entity, BindingResult result) {
         if (this.repository.findById(entity.getIdentifier()).isPresent()) {
@@ -45,16 +54,26 @@ public abstract class AbstractCrudRestController<T extends IdentifiableEntity<Ob
         return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(entity));
     }
 
+    @ApiOperation("Returns entity by name if present")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Successfully returned"),
+            @ApiResponse(code = 404, message = "Name doesn't match to any entity")
+    })
     @GetMapping(NAME_PATH + "{name}")
-    public ResponseEntity<T> getByName(@PathVariable String name) {
+    public ResponseEntity<T> getByName(@PathVariable @ApiParam("Entity name") String name) {
         return this.repository.findByName(name)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @ApiOperation("Updates entity by name")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Name doesn't match to any entity"),
+            @ApiResponse(code = 204, message = "Successfully updated")
+    })
     @IsAdmin
     @PutMapping(NAME_PATH + "{name}")
-    public ResponseEntity<T> updateByName(@RequestBody T entity, @PathVariable String name, BindingResult result) {
+    public ResponseEntity<T> updateByName(@RequestBody T entity, @PathVariable @ApiParam("Entity name") String name, BindingResult result) {
         if (!this.repository.findByName(name).isPresent()) {
             return ResponseEntity.notFound().build();
         }
@@ -67,15 +86,25 @@ public abstract class AbstractCrudRestController<T extends IdentifiableEntity<Ob
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation("Partial updates entity by name")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Name doesn't match to any entity"),
+            @ApiResponse(code = 204, message = "Successfully updated")
+    })
     @IsAdmin
     @PatchMapping(NAME_PATH + "{name}")
-    public ResponseEntity<T> partialUpdateByName(@RequestBody T entity, @PathVariable String name) throws InvocationTargetException, IllegalAccessException {
+    public ResponseEntity<T> partialUpdateByName(@RequestBody T entity, @PathVariable @ApiParam("Entity name") String name) throws InvocationTargetException, IllegalAccessException {
         return this.partialUpdateEntity(entity, this.repository.findByName(name));
     }
 
+    @ApiOperation("Deletes entity by name")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Name doesn't match to any entity"),
+            @ApiResponse(code = 204, message = "Successfully deleted")
+    })
     @IsAdmin
     @DeleteMapping(NAME_PATH + "{name}")
-    public ResponseEntity<T> removeByName(@PathVariable String name) {
+    public ResponseEntity<T> removeByName(@PathVariable @ApiParam("Entity name") String name) {
         if (!this.repository.existsByName(name)) {
             return ResponseEntity.notFound().build();
         }
@@ -84,16 +113,26 @@ public abstract class AbstractCrudRestController<T extends IdentifiableEntity<Ob
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation("Returns entity by identifier if present")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Successfully returned"),
+            @ApiResponse(code = 404, message = "Identifier doesn't match to any entity")
+    })
     @GetMapping(ID_PATH + "{identifier}")
-    public ResponseEntity<T> getByIdentifier(@PathVariable String identifier) {
+    public ResponseEntity<T> getByIdentifier(@PathVariable @ApiParam("Entity identifier") String identifier) {
         return this.repository.findById(new ObjectId(identifier))
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @ApiOperation("Updates entity by identifier")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Identifier doesn't match to any entity"),
+            @ApiResponse(code = 204, message = "Successfully updated")
+    })
     @IsAdmin
     @PutMapping(ID_PATH + "{identifier}")
-    public ResponseEntity<T> updateByIdentifier(@RequestBody T entity, @PathVariable String identifier, BindingResult result) {
+    public ResponseEntity<T> updateByIdentifier(@RequestBody T entity, @PathVariable @ApiParam("Entity identifier") String identifier, BindingResult result) {
         if (!this.repository.findById(new ObjectId(identifier)).isPresent()) {
             return ResponseEntity.notFound().build();
         }
@@ -106,15 +145,25 @@ public abstract class AbstractCrudRestController<T extends IdentifiableEntity<Ob
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation("Partial updates entity by identifier")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Identifier doesn't match to any entity"),
+            @ApiResponse(code = 204, message = "Successfully updated")
+    })
     @IsAdmin
     @PatchMapping(ID_PATH + "{identifier}")
-    public ResponseEntity<T> partialUpdateByIdentifier(@RequestBody T entity, @PathVariable String identifier) throws InvocationTargetException, IllegalAccessException {
+    public ResponseEntity<T> partialUpdateByIdentifier(@RequestBody T entity, @PathVariable @ApiParam("Entity identifier") String identifier) throws InvocationTargetException, IllegalAccessException {
         return this.partialUpdateEntity(entity, this.repository.findById(new ObjectId(identifier)));
     }
 
+    @ApiOperation("Deletes entity by identifier")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Identifier doesn't match to any entity"),
+            @ApiResponse(code = 204, message = "Successfully deleted")
+    })
     @IsAdmin
     @DeleteMapping(ID_PATH + "{identifier}")
-    public ResponseEntity<T> removeByIdentifier(@PathVariable String identifier) {
+    public ResponseEntity<T> removeByIdentifier(@PathVariable @ApiParam("Entity identifier") String identifier) {
         ObjectId id = new ObjectId(identifier);
         if (!this.repository.existsById(id)) {
             return ResponseEntity.notFound().build();
