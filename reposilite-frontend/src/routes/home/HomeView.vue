df8ab696aa785ab5c410d9cd6481d8bc2ce0c9cf
rrye <krydzewski312@gmail.com>
2018-12-16 18:07:34 +0100

    Add landing page

---
+++ reposilite-frontend/src/routes/home/HomeView.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id="home-wrapper">
+        <Navbar class="uk-navbar-transparent" />
+
+        <div class="uk-position-large uk-position-center-left">
+            <h1>Reposilite</h1>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+            <br>
+            <button href="#" class="signin-button">Sign in using Github</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Navbar from "./components/Navbar.vue";
+
+    export default {
+        name: 'HomeView',
+        components: {
+            Navbar
+        }
+    }
+</script>
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css?family=Arimo|PT+Sans');
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700,900|Raleway|Rubik:400,700,900');
+
+    h1 {
+        position: relative;
+        color: #302930;
+        text-transform: uppercase;
+        font-family: 'Montserrat', sans-serif;
+        font-weight: bold;
+    }
+
+    h2 {
+        position: relative;
+        color: #5e5e5e;
+        font-size: 15px;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 100;
+        max-width: 400px;
+    }
+
+    .signin-button {
+        position: relative;
+        width: 320px;
+        height: 45px;
+        color: white;
+        font-family: 'Rubik', sans-serif;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+        font-size: 12px;
+        background: linear-gradient(135deg, #e91653 0%, #1c7586 100%);
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        border: none;
+        border-radius: 25px;
+    }
+</style>
---
+++ reposilite-frontend/src/routes/home/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="navbar-wrapper">
+        <nav class="uk-navbar uk-navbar-container uk-navbar-transparent uk-margin">
+            <div class="uk-navbar-left">
+                <router-link class="uk-navbar-item uk-logo" to="/">
+                    <a class="uk-navbar-item uk-logo">Reposilite</a>
+                </router-link>
+            </div>
+            <div class="uk-navbar-right uk-margin-right">
+                <ul class="uk-navbar-nav uk-visible@m">
+                    <router-link to="/about" tag="li"><a>About</a></router-link>
+                    <router-link to="/contact" tag="li"><a>Contact</a></router-link>
+                </ul>
+                <a class="uk-navbar-toggle" href="#modal-full" uk-search-icon uk-toggle></a>
+            </div>
+        </nav>
+        <div id="modal-full" class="uk-modal-full uk-modal" uk-modal>
+            <div class="uk-modal-dialog uk-flex uk-flex-center uk-flex-middle" uk-height-viewport>
+                <button class="uk-modal-close-full" type="button" uk-close></button>
+                <form class="uk-search uk-search-large">
+                    <input class="uk-search-input uk-text-center" type="search" placeholder="Search..." autofocus>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<style>
+    .uk-modal-dialog {
+        opacity: 0.8;
+    }
+</style>
