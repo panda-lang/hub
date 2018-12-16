4236053d4ba0b4e11ebfcbab5dac3e71e780c062
unknown <bstefanskicom@gmail.com>
2018-12-16 21:13:15 +0100

    Implement user authentication on frontend (Closes #88)

--- reposilite-frontend/src/router/routes.js
+++ reposilite-frontend/src/router/routes.js
@@ -1,10 +1,21 @@
 import HomeComponent from "../routes/home/HomeView";
+import LoginComponent from "../routes/login/LoginView";
 
-export const routes = [{
-    path: '/',
-    name: 'HomeView',
-    component: HomeComponent,
-    meta: {
-        requiresAuth: false
+export const routes = [
+    {
+        path: "/",
+        name: "HomeView",
+        component: HomeComponent,
+        meta: {
+            requiresAuth: false
+        }
+    },
+    {
+        path: "/login",
+        name: "LoginView",
+        component: LoginComponent,
+        meta: {
+            requiresAuth: false
+        }
     }
-}];
+];
