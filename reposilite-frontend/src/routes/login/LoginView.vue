aaf4923ca71c258598e4cba3098077df46f0459b
bmstefanski <bstefanskicom@gmail.com>
2018-12-17 23:52:51 +0100

    Add validation to login & registration form (Closes #91)

--- reposilite-frontend/src/routes/login/LoginView.vue
+++ reposilite-frontend/src/routes/login/LoginView.vue
@@ -1,18 +1,28 @@
 <template>
     <form @submit.prevent="performLogin">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" v-model="username" required/>
+        <div class="uk-grid">
+            <div class="uk-width-1-3">
+                <input class="uk-input" type="text" id="username" name="username" placeholder="Username" v-model="username"/>
+                <div style="color: red" v-if="!$v.username.required">Username is required</div>
+            </div>
 
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" v-model="password" required/>
+            <div class="uk-width-1-3">
+                <input class="uk-input" type="password" name="password" id="password" placeholder="Password" v-model="password"/>
+                <div style="color: red" v-if="!$v.password.required">Password is required</div>
+            </div>
 
-        <button type="submit">Login</button>
+            <div class="uk-width-1-3">
+                <button class="uk-button uk-button-secondary" type="submit" :disabled="isInvalid">Login</button>
+            </div>
+        </div>
+
     </form>
 </template>
 
 <script>
     import {userService} from "../../services/user.service.js";
     import {notification} from "../../utils/toastr.utils";
+    import {required} from "vuelidate/lib/validators/index";
 
     export default {
         name: "LoginView",
@@ -25,18 +35,23 @@
         created: () => {
             userService.logout();
         },
+        validations: {
+            username: {required},
+            password: {required}
+        },
         methods: {
             performLogin() {
-                if (!(this.username && this.password)) {
-                    return;
-                }
-
                 userService.login(this.username, this.password)
                     .then(() => {
                         notification.success("Successfully logged in!");
                         this.$router.push("/")
                     })
                     .catch(reason => notification.error(reason));
+            }
+        },
+        computed: {
+            isInvalid() {
+                return this.$v.$invalid;
             }
         }
     }
