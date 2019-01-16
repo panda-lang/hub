546c258de66468bc82293bbabb46367fed40713e
bmstefanski <bstefanskicom@gmail.com>
2019-01-16 15:52:09 +0100

    Move organization validation messages to messages.properties

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/organization/OrganizationController.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/organization/OrganizationController.java
@@ -3,7 +3,6 @@ package org.panda_lang.reposilite.organization;
 import io.swagger.annotations.ApiOperation;
 import org.bson.types.ObjectId;
 import org.panda_lang.reposilite.utils.AbstractCrudController;
-import org.panda_lang.reposilite.utils.CrudOperationsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/organization/")
 public class OrganizationController extends AbstractCrudController<Organization, ObjectId, OrganizationUpdateDto, OrganizationUpdateDto> {
 
-    // TODO Organization update and create DTOs
+    // TODO Organization create DTO
     protected OrganizationController(@Autowired OrganizationService service) {
         super(service);
     }
