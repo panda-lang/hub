1ee4315ad0b924fed5ffd965d4010210feab129c
bmstefanski <bstefanskicom@gmail.com>
2019-01-16 11:01:41 +0100

    Remove abstract crud rest controller & add new BeanUtils impl

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/utils/AbstractDto.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/utils/AbstractDto.java
@@ -1,9 +1,15 @@
 package org.panda_lang.reposilite.utils;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public abstract class AbstractDto<T extends IdentifiableEntity<?>> {
+
+    @JsonIgnore
+    public String getName() {
+        throw new UnsupportedOperationException();
+    }
 
     public T toEntity() {
         throw new UnsupportedOperationException();
