d3951cb7e9e51dfc1a19cb16fd8d4560c3c8f622
bmstefanski <bstefanskicom@gmail.com>
2019-01-16 11:01:41 +0100

    Remove abstract crud rest controller & add new BeanUtils impl

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/user/UserController.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/user/UserController.java
@@ -1,7 +1,6 @@
 package org.panda_lang.reposilite.user;
 
 import io.swagger.annotations.ApiOperation;
-import org.panda_lang.reposilite.utils.AbstractCrudRestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @ApiOperation("Operations pertaining to user")
 @RestController
 @RequestMapping("api/user/")
-public class UserController extends AbstractCrudRestController<User> {
-
-    @Autowired
-    public UserController(UserRepository userRepository) {
-        super(userRepository);
-    }
+public class UserController {
 
 }
