fabac090e11ca68d7fec6bc51bceaf53d0ab9f9a
bmstefanski <bstefanskicom@gmail.com>
2019-01-16 17:23:18 +0100

    Replace authentication endpoint with user details endpoint (Closes #109)

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/user/UserController.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/user/UserController.java
@@ -4,16 +4,25 @@ import io.swagger.annotations.ApiOperation;
 import org.bson.types.ObjectId;
 import org.panda_lang.reposilite.utils.AbstractCrudController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @ApiOperation("Operations pertaining to user")
 @RestController
-@RequestMapping("api/user/")
+@RequestMapping("/api/users")
 public class UserController extends AbstractCrudController<User, ObjectId, UserUpdateDto, UserRegistrationDto> {
 
     protected UserController(@Autowired UserCrudService service) {
         super(service);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/me")
+    public ResponseEntity<User> me(@AuthenticatedUser User user) {
+        return ResponseEntity.ok(user);
     }
 
 }
