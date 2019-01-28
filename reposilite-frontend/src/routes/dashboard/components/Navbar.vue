91cfc5aee3f7aa8732db00d45a17fbdbc4e787c5
rrye <krydzewski312@gmail.com>
2019-01-28 13:30:52 +0100

    Basic dashboard layout

---
+++ reposilite-frontend/src/routes/dashboard/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+    #navbar-wrapper
+        nav.uk-navbar.uk-navbar-container.uk-navbar-transparent.uk-margin
+            .uk-navbar-left
+                router-link.uk-navbar-item.uk-logo(to='/')
+                    a.uk-navbar-item.uk-logo Reposilite
+</template>
+
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import url('https://fonts.googleapis.com/css?family=Titillium+Web')
+
+    .uk-navbar
+        background-color white
+        -webkit-box-shadow 0px 0px 71px -8px rgba(0,0,0,0.23)
+        -moz-box-shadow 0px 0px 71px -8px rgba(0,0,0,0.23)
+        box-shadow 0px 0px 71px -8px rgba(0,0,0,0.23)
+
+    .uk-navbar-item
+        font-family 'Titillium Web', sans-serif
+</style>
