69d2cefc6e809bbf6228b5acc87e88d8fcc16dd5
Kasper Seweryn <casper@wvffle.net>
2019-02-10 14:29:48 +0100

    Add suggestion auto hide when input is empty
    I think this is how this should be done. Before you were printing out
    every single search object.

--- reposilite-frontend/src/components/Search.vue
+++ reposilite-frontend/src/components/Search.vue
@@ -36,7 +36,8 @@
         },
         methods: {
             onInputChange(text) {
-                if (text === null) {
+                if (!text) {
+                    this.filteredOptions = []
                     return;
                 }
 
