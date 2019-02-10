c3b2ef4ea4d641ecbdc65d49b391c3567bbd2277
Kasper Seweryn <casper@wvffle.net>
2019-02-10 14:03:30 +0100

    Add clean route declaration

--- reposilite-frontend/src/router/routes.js
+++ reposilite-frontend/src/router/routes.js
@@ -3,37 +3,13 @@ import LoginView from '../routes/login/LoginView';
 import RegisterView from '../routes/register/RegisterView';
 import DashboardView from '../routes/dashboard/DashboardView'
 
+const route = (path, component, meta = {}) => ({
+    path, component, name: component.name, meta
+})
+
 export const routes = [
-    {
-        path: '/',
-        name: 'HomeView',
-        component: HomeView,
-        meta: {
-            requiresAuth: false
-        }
-    },
-    {
-        path: '/login',
-        name: 'LoginView',
-        component: LoginView,
-        meta: {
-            requiresAuth: false
-        }
-    },
-    {
-        path: '/register',
-        name: 'RegisterView',
-        component: RegisterView,
-        meta: {
-            requiresAuth: false
-        }
-    },
-    {
-        path: '/dashboard',
-        name: 'DashboardView',
-        component: DashboardView,
-        meta: {
-            requiresAuth: false
-        }
-    }
+    route('/', HomeView),
+    route('/login', LoginView),
+    route('/register', RegisterView),
+    route('/dashboard', DashboardView),
 ];
