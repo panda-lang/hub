db3db3648f322abad8469e874e1107d7e69bc375
rrye <44753666+rrye@users.noreply.github.com>
2019-02-17 14:27:34 +0100

    Add error page (#136)
    * Add error page
    
    * Add navbar to error page
    
    * Add unicode arrow

--- reposilite-frontend/src/router/routes.js
+++ reposilite-frontend/src/router/routes.js
@@ -1,4 +1,5 @@
 import HomeView from '@/views/landing/home/HomeView'
+import ErrorView from '@/views/landing/error/ErrorView'
 import LoginView from '@/views/login/LoginView'
 import RegisterView from '@/views/register/RegisterView'
 import DashboardView from '@/views/dashboard/DashboardLayout'
@@ -9,6 +10,7 @@ const route = (path, component, data = {}, meta = {}) => ({
 
 export const routes = [
     route('/', HomeView),
+    route('*', ErrorView),
     route('/login', LoginView),
     route('/register', RegisterView),
     route('/dashboard', DashboardView)
