01c885a3bba814fe1c749fb30b54dbbf0cd14403
rrye <krydzewski312@gmail.com>
2019-02-17 12:23:24 +0100

    Add navbar to error page

---
+++ reposilite-frontend/src/views/landing/error/component.js
@@ -0,0 +1,14 @@
+import Navbar from '@/components/navbar/Navbar.vue'
+
+export default {
+    name: 'ErrorView',
+    components: {
+        Navbar
+    },
+    data() {
+        return {
+            userAvatar: 'https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP2538-CUSA05620_00-AV00000000000118//image?_version=00_09_000&platform=chihiro&w=720&h=720&bg_color=000000&opacity=100',
+            username: 'awesome user 🐢'
+        }
+    }
+}
