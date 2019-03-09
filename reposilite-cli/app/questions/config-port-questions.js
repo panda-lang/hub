ad2d2db35bf1c16692607f9e3d8289837d304494
dzikoysk <dzikoysk@dzikoysk.net>
2019-03-09 15:17:49 +0100

    Read/write prepared configuration, add backend-port question

---
+++ reposilite-cli/app/questions/config-port-questions.js
@@ -0,0 +1,8 @@
+module.exports = {
+    type: 'number',
+    name: 'backend-port',
+    message: 'Choose the port number for backend',
+    initial: 3000,
+    min: 0,
+    max: 65535
+}
--- reposilite-cli/app/reposilite-config.js
+++ reposilite-cli/app/reposilite-config.js
@@ -1,12 +1,31 @@
+const fs = require('fs')
 const prompts = require('prompts')
+const questions = require('./reposilite-questions')
 
-const questions = [
-    require('./questions/variant-question')
-]
+const configPath = 'config.yml'
 
 async function initialize() {
-    const response = await prompts(questions)
-    console.log(response)
+    const init = await prompts(questions.init)
+    let config;
+
+    if (fs.existsSync(configPath)) {
+        config = JSON.parse(fs.readFileSync(configPath))
+    }
+    
+    if (config == null) {
+        config = await prompts(questions.config)
+
+        fs.writeFileSync(configPath, JSON.stringify(config), function (err) {
+            if (err) {
+                console.log('Cannot save file! Caused by: ' + err)
+                process.exit()
+            }
+        })
+    }
+
+    console.log("Results:")
+    console.log(init)
+    console.log(config)
 }
 
 module.exports = { initialize }
