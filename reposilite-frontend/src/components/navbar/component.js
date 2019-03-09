36997138d4f54bb32e1e65b9440410af5260b1af
rrye <44753666+rrye@users.noreply.github.com>
2019-03-09 12:03:25 +0100

    Add search component to dashboard navbar (#147)

--- reposilite-frontend/src/components/navbar/component.js
+++ reposilite-frontend/src/components/navbar/component.js
@@ -1,5 +1,10 @@
+import Search from '@/components/search/Search'
+
 export default {
     name: 'Navbar',
+    components: {
+        Search
+    },
     data() {
         return {
             userAvatar: 'https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP2538-CUSA05620_00-AV00000000000118//image?_version=00_09_000&platform=chihiro&w=720&h=720&bg_color=000000&opacity=100',
