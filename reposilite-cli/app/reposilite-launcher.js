219d6234d4fd9c5263c8c19d3b26a03a4428a591
dzikoysk <dzikoysk@dzikoysk.net>
2019-05-18 21:59:41 +0200

    Improve logging, add help, b and f commands, fixes

--- reposilite-cli/app/reposilite-launcher.js
+++ reposilite-cli/app/reposilite-launcher.js
@@ -20,29 +20,45 @@ async function launch (config) {
     }
 
     stdin.addListener('data', (value) => {
-        executeCommand(value.toString().trim())
+        setTimeout(() => executeCommand(value.toString().trim()), 50)
     })
 
     await promiseWhileDelayed(() => backend && backend.isActive() || frontend && frontend.isActive(), 250)
 }
 
 function executeCommand(command) {
-    switch (command) {
-        case 'stop':
+    switch (true) {
+        case 'stop' === command:
+            console.log("Shutting down...")
             frontend.shutdown()
             backend.shutdown()
             stdin.pause()
+            return
+        case 'status' === command:
+            console.log(" • Reposilite Frontend: " + (frontend ? color.green("Active") : color.red("Inactive")))
+            console.log(" • Reposilite Backend: " + (backend ? color.green("Active") : color.red("Inactive")))
             break
-        case 'status':
-            console.log("• Reposilite Frontend: " + (frontend ? color.green("Active") : color.red("Inactive")))
-            console.log("• Reposilite Backend: " + (backend ? color.green("Active") : color.red("Inactive")))
+        case command.startsWith('f '):
+            frontend.execute(command.replace("f ", "").trim())
             break
-        case '':
+        case command.startsWith('b '):
+            backend.execute(command.replace("b ", "").trim())
+            break
+        case 'help' === command:
+            console.log(' Reposilite CLI Commands:')
+            console.log('    stop - stop the services and cli')
+            console.log('    status - check status of services')
+            console.log('    f <frontend-command> - execute command in the frontend service')
+            console.log('    b <backend-command> - execute command in the backend service')
+            break
+        case '' === command:
             break
         default:
             console.warn("Unknown command: " + command)
             break
     }
+
+    process.stdout.write(":> ")
 }
 
 module.exports = { launch }
