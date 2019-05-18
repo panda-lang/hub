a9e587666369da692674431b88ee286cb0f8a2d6
dzikoysk <dzikoysk@dzikoysk.net>
2019-05-18 18:54:54 +0200

    Fix invalid ids

--- reposilite-cli/app/questions/init-variant-question.js
+++ reposilite-cli/app/questions/init-variant-question.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'variant',
     message: 'Select the services to launch',
     choices: [
-        { title: 'Reposilite Backend (server)', value: 'frontend' },
+        { title: 'Reposilite Backend (server)', value: 'backend' },
         { title: 'Reposilite Backend (server) + Frontend (website)', value: 'full' }
     ],
     initial: 1
