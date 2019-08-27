58a179a07db851791ecbe4b1624143828ad23d2f
dzikoysk <dzikoysk@dzikoysk.net>
2019-08-27 02:01:20 +0200

    GH-209 Fix invalid paths in maven depository integration tests

--- reposilite-backend/src/test/java/org.panda_lang.reposilite/depository/maven/ArtifactControllerIntegrationTest.java
+++ reposilite-backend/src/test/java/org.panda_lang.reposilite/depository/maven/ArtifactControllerIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,6 +21,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class ArtifactControllerIntegrationTest {
+
+    @Autowired
+    @Qualifier("workspaceDirectory")
+    private File workspace;
 
     @Autowired
     private MavenDepositoryController mavenDepositoryController;
@@ -43,7 +48,7 @@ class ArtifactControllerIntegrationTest {
 
         this.mockMvc.perform(MockMvcRequestBuilders.get("/api/repository/maven/releases/org/panda-lang/panda-utilities/"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.path", Matchers.is(path)));
+                .andExpect(jsonPath("$.path", Matchers.is(workspace + File.separator + path)));
     }
 
 }
