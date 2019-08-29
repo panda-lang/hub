c9badd1c8eeff4a6cb16f8225ddb789ce5e6149b
dzikoysk <dzikoysk@dzikoysk.net>
2019-08-29 20:17:47 +0200

    GH-209 Support Content-Length by ResponseUtils#returnFile

--- reposilite-backend/src/main/java/org/panda_lang/reposilite/utils/ResponseUtils.java
+++ reposilite-backend/src/main/java/org/panda_lang/reposilite/utils/ResponseUtils.java
@@ -13,6 +13,7 @@ public final class ResponseUtils {
     public static ResponseEntity<Object> returnFile(HttpServletResponse response, String contentType, File file) throws IOException {
         response.addHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
         response.addHeader("Content-Type", contentType);
+        response.addHeader("Content-Length", Long.toString(file.length()));
 
         IOUtils.copy(new FileSystemResource(file).getInputStream(), response.getOutputStream());
         response.flushBuffer();
